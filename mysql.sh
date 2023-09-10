40e1d9575aa088b8092260136f6aa2977103fb6a
Yaminipoojitha36 <yaminijanakiram01@gmail.com>
2023-09-10 14:57:08 +0530

    Roboshop scripts

---
+++ mysql.sh
@@ -0,0 +1,6 @@
+cp mysql.repo /etc/syum.repos.d/mysql.repo
+yum module disable mysql -y
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl restart mysqld
+mysql_secure_installation --set-root-pass RoboShop@1
